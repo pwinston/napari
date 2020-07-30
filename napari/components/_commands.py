3d40125c25f9597117277e60f8b6979a7e7d66b3
Philip Winston <pwinston@gmail.com>
2020-07-30 11:23:15 -0400

    Working commands: list and inspect

--- napari/components/_commands.py
+++ napari/components/_commands.py
@@ -1,6 +1,73 @@
 """Commands class for IPython console.
 """
-from ..utils.text_table import TextTable
+from ..utils.text_table import TextTable, red
+
+HELP_STR = f"""
+{red("Available Commands:")}
+cmd.help
+cmd.list
+cmd.inspect(layer_id)
+"""
+
+
+class ListLayersTable:
+    """Table showing the layers and their names, types and shapes.
+
+    """
+
+    def __init__(self, layers):
+        self.layers = layers
+        self.table = TextTable(["ID", "NAME", "TYPE", "SHAPE"])
+        for i, layer in enumerate(self.layers):
+            self._add_row(i, layer)
+
+    def _get_shape_str(self, data):
+        """Get shape string for the data.
+
+        For example:
+            (10, 100, 100)
+            (10, 10000, 10000) (5 levels)
+            NONE
+        """
+        if isinstance(data, list):
+            if len(data) == 0:
+                return "NONE"  # e.g. Shape layer
+            else:
+                # Multi-scale
+                return f"{data[0].shape} ({len(data)} levels)"  # Multi-scale
+        else:
+            return str(data.shape)
+
+    def _add_row(self, i, layer):
+        """Add one row to the layer list table."""
+        layer_type = type(layer).__name__
+        shape_str = self._get_shape_str(layer.data)
+        self.table.add_row([i, layer.name, layer_type, shape_str])
+
+    def print(self):
+        """Print the whole table."""
+        self.table.print()
+
+
+class InspectLayerTable:
+    """Table showing the levels in a single layer.
+
+    """
+
+    def __init__(self, layer_id, layer):
+        self.layer_id = layer_id
+        self.layer = layer
+        self.table = TextTable(["LEVEL", "SHAPE"])
+        data = layer.data
+        if isinstance(data, list):
+            for i, level in enumerate(data):
+                shape_str = level.shape if level.shape else "NONE"
+                self.table.add_row([i, shape_str])
+
+    def print(self):
+        """Print the whole table."""
+
+        self.table.print()
 
 
 class ConsoleCommands:
@@ -15,24 +82,29 @@ class ConsoleCommands:
     def __init__(self, layerlist):
         self.layers = layerlist
 
+    def __repr__(self):
+        return HELP_STR
+
     @property
     def help(self):
-        """Print the commands we understand."""
-        print("Commands:")
-        print("cmd.help")
-        print("cmd.list")
+        print(HELP_STR)
 
     @property
     def list(self):
         """Print the current list of layers."""
-        table = TextTable(
-            ["ID", "TYPE", "MULTI", "SHAPE", "NAME"], [3, 8, 7, 0, 20]
-        )
-
-        for i, layer in enumerate(self.layers):
-            layer_type = type(layer).__name__
-            data_shape = layer.data.shape
-            table.add_row(
-                [i, layer_type, layer.multiscale, data_shape, layer.name]
-            )
+        table = ListLayersTable(self.layers)
         table.print()
+
+    def inspect(self, layer_id):
+        """Inspect a single layer"""
+        try:
+            layer = self.layers[layer_id]
+        except KeyError:
+            print(f"Invalid layer index: {layer_id}")
+            return
+
+        num_levels = len(layer.data)
+        header = f"ID: {layer_id} {layer.name} has {num_levels} levels"
+        print(red(header))
+        if num_levels > 0:
+            InspectLayerTable(layer_id, layer).print()
--- napari/utils/text_table.py
+++ napari/utils/text_table.py
@@ -1,14 +1,13 @@
 """TextTable class.
-
-Notes
------
-We could use an external package like colorit, printy, etc. for colors
-but now just doing it by hand.
 """
-from typing import List
+from typing import List, Optional
 
 
-def _red(string):
+# We could use an external package like colorit, printy, etc. for colors
+# but now just doing it by hand.
+
+
+def red(string):
     return f"\x1b[31m{string}\x1b[0m"
 
 
@@ -31,17 +30,17 @@ class TextTable:
     headers : List[str]
         The column headers such as  ["NAME", "AGE"].
 
-    widths: List[int]
-        The column widths such as [10, 5]. Where 0 means auto-size.
+    widths: Optional[List[int]]
+        Use these widths instead of automatic widths, 0 means auto for that column.
     """
 
     # For auto-width columns, pad max width by this many columns to
     # leave a little room between columns.
     PADDING = 2
 
-    def __init__(self, headers: List[str], widths: List[int]):
+    def __init__(self, headers: List[str], widths: List[int] = None):
         self.headers: List[str] = headers
-        self.widths: List[int] = widths
+        self.widths: Optional[List[int]] = widths
         self.rows: List[list] = []
 
     def add_row(self, row: List[str]):
@@ -54,15 +53,31 @@ class TextTable:
         """
         self.rows.append(row)
 
-    def get_width(self, index: int) -> int:
-        """Return max width of the column at the given index."""
-        return max([len(str(x)) for x in self.rows[index]])
+    def _get_max_data_width(self, column_index: int) -> int:
+        """Return maximum width of this column in the data."""
+        if self.rows:
+            return max([len(str(row[column_index])) for row in self.rows])
+        return 0
+
+    def _get_width(self, column_index: int) -> int:
+        """Return column width at the given index.."""
+        if self.widths is None or self.widths[column_index] == 0:
+            # Auto sized column look at the data.
+            data_width = self._get_max_data_width(column_index)
+
+            # Use wider of data or the header, plus some padding.
+            header_width = len(self.headers[column_index])
+            return max(data_width, header_width) + self.PADDING
+        else:
+            # Fixed width column.
+            return self.widths[column_index]
 
     @property
     def header_str(self):
         """Print the header of the table with the column names."""
         header_str = ""
-        for heading, width in zip(self.headers, self.widths):
+        for i, heading in enumerate(self.headers):
+            width = self._get_width(i)
             header_str += f"{str(heading):<{width}}"
         return header_str
 
@@ -70,14 +85,12 @@ class TextTable:
         """Print the rows of the table."""
         row_str = ""
         for i, value in enumerate(row):
-            width = self.widths[i]
-            if width == 0:
-                width = self.get_width(i) + self.PADDING
+            width = self._get_width(i)
             row_str += f"{str(value):<{width}}"
         return row_str
 
     def print(self):
         """Print the entire table: header line plus the rows."""
-        print(_red(self.header_str))
+        print(red(self.header_str))
         for row in self.rows:
             print(self.row_str(row))
