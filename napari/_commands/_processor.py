702b78d12c1a87a66e8c892007c18078869bc8a9
Philip Winston <pwinston@gmail.com>
2020-08-11 09:43:33 -0400

    remove viewer.cmd.layers and viewer.cmd.levels
    We are unsure about the fate these more generic commands and are focusing just on async
    command for now in this branch

--- napari/_commands/_processor.py
+++ napari/_commands/_processor.py
@@ -1,15 +1,13 @@
 """ConsoleProcessor class for our IPython console.
 """
-from ._utils import highlight
-from ._layers import LayerCommands
 from ._loader import LoaderCommands
+from ._utils import highlight
 
 HELP_STR = f"""
 {highlight("Available Commands:")}
 cmd.help
 cmd.loader
-cmd.layers
-cmd.levels(layer_id)
+cmd.loader_config
 """
 
 
@@ -20,7 +18,6 @@ class CommandProcessor:
     """
 
     def __init__(self, layerlist):
-        self.layer_commands = LayerCommands(layerlist)
         self.loader_commands = LoaderCommands(layerlist)
 
     def __repr__(self):
@@ -29,15 +26,6 @@ class CommandProcessor:
     @property
     def help(self):
         print(HELP_STR)
-
-    @property
-    def layers(self):
-        """Print a table with the current layers."""
-        return self.layer_commands.layers
-
-    def levels(self, layer_id):
-        """Print information about a single layer."""
-        return self.layer_commands.levels(layer_id)
 
     @property
     def loader(self):
