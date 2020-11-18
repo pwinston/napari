0959c176e1572f800366cb1200b55cd5397506e1
Philip Winston <pwinston@gmail.com>
2020-11-18 17:12:23 -0500

    Shorten function name; slight cleanup w/ zoom

--- napari/layers/image/experimental/__init__.py
+++ napari/layers/image/experimental/__init__.py
@@ -3,7 +3,8 @@
 from .octree_chunk import OctreeChunk, OctreeChunkGeom
 from .octree_intersection import OctreeIntersection
 from .octree_level import OctreeLevel
-from .octree_tile_builder import create_multi_scale_from_image
+from .octree_tile_builder import create_multi_scale
 from .octree_util import SliceConfig, TestImageSettings
 
-# from .octree_image import OctreeImage  # circular
+# Can't do this because it's circular right now...
+# from .octree_image import OctreeImage
